221028dbae80cdbe8a338fd3d64d7f3da7e6c714
harrisonychung <70103285+harrisonychung@users.noreply.github.com>
2022-08-04 18:54:08 -0400

    pokepedia functionality added, can search a pokemon + result is outputted

--- pokepedia/src/App.js
+++ pokepedia/src/App.js
@@ -1,12 +1,59 @@
 import './App.css';
+import {useState} from "react";
+import Axios from "axios";
 
 function App() {
+
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "", 
+    species: "", 
+    img: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    type: "",
+  });
+
+
+
+  const searchPokemon =  () => {
+      Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+        (response) => {
+        setPokemon({
+          name: pokemonName, 
+          species: response.data.species.name, 
+          img: response.data.sprites.front_default,
+          hp: response.data.stats[0].base_stat,
+          attack: response.data.stats[1].base_stat,
+          defense: response.data.stats[2].base_stat,
+          type: response.data.types[0].type.name,})
+      });
+      setPokemonChosen(true);
+  }
   return (
     <div className="App">
       <div className="TitleSection">
       <h1>Pokepedia</h1>
-      <input type="text"/>
-      <button>Search Pokemon</button>
+      <input type="text" onChange={(event) => {
+        setPokemonName(event.target.value)}}/>
+      <button onClick={searchPokemon}>Search Pokemon</button>
+    </div>
+    <div className="Display">
+        {! pokemonChosen ? (
+          <h1>Please choose a Pokemon</h1>
+        ) : (
+          <>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.img} alt="pokemon_image"/>
+            <h3>Species: {pokemon.species}</h3>
+            <h3>Type: {pokemon.type}</h3>
+            <h5>Health Points: {pokemon.hp}</h5>
+            <h5>Attack Points: {pokemon.attack}</h5>
+            <h5>Defense Points: {pokemon.defense}</h5>
+          </>
+        )}
     </div>
   </div>
   );
