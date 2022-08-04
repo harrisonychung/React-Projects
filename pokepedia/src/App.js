47232cfaa0dcbf792e4dc32493cc4e14d3cd03b2
harrisonychung <70103285+harrisonychung@users.noreply.github.com>
2022-08-04 16:44:56 -0400

    pokepedia project being pushed, added basic styling

---
+++ pokepedia/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="TitleSection">
+      <h1>Pokepedia</h1>
+      <input type="text"/>
+      <button>Search Pokemon</button>
+    </div>
+  </div>
+  );
+}
+
+export default App;
